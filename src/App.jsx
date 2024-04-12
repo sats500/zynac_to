a2a290f3c106a6fe60708dfa4786f54d815bfa70
Satakshi <77228717+sats500@users.noreply.github.com>
2024-04-12 17:31:57 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading";
+import TodoList from "./components/ToDoList";
+
+function App() {
+  return ( 
+  <div className="App font-Poppins container py-16 px-6 min-h-screen mx-auto">
+  <Heading/>
+  <TodoList/>
+  </div>
+  );
+
+}
+
+export default App
+
